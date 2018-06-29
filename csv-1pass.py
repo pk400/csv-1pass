64500a5ce98ea3099e64c7ef58560842cea7a912
Joel Aro <joelaro.to@gmail.com>
2018-06-28 22:58:45 -0400

    Initial commit

---
+++ csv-1pass.py
@@ -0,0 +1,28 @@
+# This script converts the csv file output by the ff-password-exporter tool
+# to a csv file that can be imported into 1Password. 1Password currently can
+# only import csv files that follow a specific format based on it's type.
+# For example, for Login types:
+# title,website,username,password,notes,custom field 1,custom field 2
+import csv, sys
+src_file = sys.argv[1]
+dest_file = sys.argv[2]
+
+file_out = open(dest_file, 'w')
+
+with open(src_file, 'rb') as csvfile:
+    out = csv.reader(csvfile, delimiter=',')
+    for row in out:
+        title = ''
+        if row[0].find('https://') == 1:
+            title = row[0].replace('https://', '').replace('.com', '')
+        elif row[0].find('http://') == 1:
+            title = row[0].replace('htts://', '').replace('.com', '')
+        comment = 'Converted from csv-1pass'
+        line = title + ',' + row[0] + ',' + row[1] + \
+                ',' + row[2] + ',' + comment + ',,\n'
+        try:
+            file_out.write(line)
+        except Exception as e:
+            print type(e)
+            print str(e)
+file_out.close()
